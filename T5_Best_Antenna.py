16e273c1ff1e1912334a22cb914ee16e3807fc92
Jack Tillman <tillman.100@buckeyemail.osu.edu>
2024-02-06 19:20:53 -0500

    Create T5_Best_Antenna.py

---
+++ T5_Best_Antenna.py
@@ -0,0 +1,8 @@
+#T5_Best_Antenna
+
+#This python script searches through the data from a GENETIS loop and outputs the top 5 antenna in the form
+#of a .txt file
+
+#Authors: Jack Tillman, Josh Lahr
+#Initial Creation: 02/06/2024
+
