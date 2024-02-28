bef1aa836f64815c7f36c37dbe50ba262fc9f596
Jack Tillman <tillman.100@buckeyemail.osu.edu>
2024-02-27 19:34:37 -0500

    fitness_score_sorting script

---
+++ fitness_score_sorting.py
@@ -0,0 +1,17 @@
+## File name: fitness_score_sorting.py
+## Authors: Jack Tillman (tillman.100@osu.edu), Josh Lahr (lahr.23@osu.edu)
+## Date: 12/06/2021
+## Purpose:
+##     This script is designed to read the fitness scores of all of the individuals in every generation
+##     after the genetic algorithm (the loop) is ran and output the top 5 individuals along with their fitness 
+##     scores
+##
+## Instructions:
+##              To run, give the following arguments:
+##              directory of generation data
+##                      
+##
+## Example:
+##              python fitness_score_sorting.py generation_data_directory
+##                      This will find the top 5 antenna once the loop is finished running
+
