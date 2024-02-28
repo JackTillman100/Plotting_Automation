7d4192f4cb7acf038e005683455e056c4a024118
Jack Tillman <tillman.100@buckeyemail.osu.edu>
2024-02-27 19:37:56 -0500

    Update fitness_score_sorting.py

--- fitness_score_sorting.py
+++ fitness_score_sorting.py
@@ -15,3 +15,14 @@
 ##              python fitness_score_sorting.py generation_data_directory
 ##                      This will find the top 5 antenna once the loop is finished running
 
+
+### Appends all of the fitness scores to a list
+##      First open fitness scores for 1 generation and append that to its own list
+##      Append that list onto the main list so that the main list has the following indexes:
+##                          fitnessScores[genNumber][individualNumber]
+
+##Sort through the main list to find the top 5 fitness scores
+
+##Print the fitness scores and the individual they're associated with in the following format:
+##      Individual: Fitness Score
+##      Ex. 09_15: 5.36201
