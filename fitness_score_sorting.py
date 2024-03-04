fe281bbf30fe07d00329118937ee1ce26dc38145
jlahr23 <148885369+jlahr23@users.noreply.github.com>
2024-03-04 14:10:20 -0500

    Update fitness_score_sorting.py
    Added the sorting mechanism to put all the fitness data into a dictionary organized by the generations

--- fitness_score_sorting.py
+++ fitness_score_sorting.py
@@ -27,3 +27,61 @@
 ##      Individual: Fitness Score
 ##      Ex. 09_15: 5.36201
 
+##################################################################################
+######## Puts all fitness scores in a dictionary organized by generations ########
+##################################################################################
+
+import numpy as np
+import argparse
+import csv
+
+## Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("source", help="Name of source folder from home directory", type=str)
+parser.add_argument("num_gens", help="Number of generations (ex:16)", type=int)
+parser.add_argument("num_individuals", help="Number of individuals (ex:16)", type=int)
+parser.add_argument("filename", help="Name of saved image", type=str)
+g=parser.parse_args()
+
+is_single_line=False
+
+##############
+
+## Declare an empty list that will hold the fitness scores 
+fitness_scores = [0, 0, 0, 0, 0]
+gen_number = [0, 0, 0, 0, 0]
+indiv_number = [0, 0, 0, 0, 0]
+best_DNA = [[0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0]]
+            
+all_fitness_scores = []
+all_indices = []
+all_gen = []
+
+
+organized_fitness_scores = {}
+
+for gen in range(0, g.num_gens - 1):
+
+    fitness_scores=[]
+    with open(g.source  + '/' + "Generation_" + str(gen) + '/' + str(gen) + "_vEffectives.csv") as f:
+        for row in f:
+            if row[0].isdigit():
+                
+                row = row.split(',')
+                
+                ### Each fitness score is appended to a list
+                fitness_scores.append(float(row[0]))
+    
+    organized_fitness_scores[gen] = fitness_scores
+            
+print('Test:')
+print('=================================================================')
+print()
+print(organized_fitness_scores)
+
+
+
